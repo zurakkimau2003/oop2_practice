90a3519b2ef11c9264d63cd151419ca18576b6f9
zurakkimau2003 <yudai11232003@gmail.com>
2021-12-09 14:08:21 +0900

    update git->github

--- modules/SayHello.py
+++ modules/SayHello.py
@@ -1,7 +1,8 @@
 import modules.SayHello as App
+
 def run():
-    app = App.SayHello("Git")
+    app=App.SayHello("Github")
     app.say()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     run()
